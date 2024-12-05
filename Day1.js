1f1a66e638e862d7fe8c8e2b2c0802355d36c424
Justsharon <odiwasharon@gmail.com>
2024-12-05 11:09:05 +0300

    properly handle errors
    Signed-off-by: Justsharon <odiwasharon@gmail.com>

--- Day1.js
+++ Day1.js
@@ -1,34 +1,41 @@
 const fs = require("fs");
 fs.readFile("01.txt", "utf-8", (err, data) => {
-    const arr = data.split("\n").map((row) => row.split("  "))
-    console.log(arr);
-    const left = arr.map(([leftArray, rightArray]) => +leftArray).sort();
-    const right = arr.map(([leftArray, rightArray]) => +rightArray).sort();
-
-    let sum = 0;
-    for (let i = 0; i < left.length; i++) {
-        sum += Math.abs(right[i] - left[i]);
+  if (err) {
+    if (err.code === "ENOENT") {
+      console.error("File not found:", err.path);
+    } else {
+      console.error("Error reading file:", err);
     }
-    console.log(sum);
+  }
+  const arr = data.split("\n").map((row) => row.split("  "));
+  console.log(arr);
+  const left = arr.map(([leftArray, rightArray]) => +leftArray).sort();
+  const right = arr.map(([leftArray, rightArray]) => +rightArray).sort();
 
-   let totalSum = 0;
-   left.forEach(val => {
-    const occurreences = right.filter(num => num === val).length;
-    totalSum += val * occurreences
-   });
+  let sum = 0;
+  for (let i = 0; i < left.length; i++) {
+    sum += Math.abs(right[i] - left[i]);
+  }
+  console.log(sum);
 
-   console.log("Total Sum:", totalSum);
+  let totalSum = 0;
+  left.forEach((val) => {
+    const occurreences = right.filter((num) => num === val).length;
+    totalSum += val * occurreences;
+  });
 
-// sum = 0;
-// for (let i = 0; i < left.length; i++) {
-//     const number = left[i];
-//     const leftIndex = right.indexOf(number);
-//     if(leftIndex === -1) {
-//         continue
-//     }
+  console.log("Total Sum:", totalSum);
 
-//     const rightIndex = right.lastIndexOf(number);
-//     sum += (rightIndex - leftIndex + 1) * number
-// }
-// console.log(sum)
+  // sum = 0;
+  // for (let i = 0; i < left.length; i++) {
+  //     const number = left[i];
+  //     const leftIndex = right.indexOf(number);
+  //     if(leftIndex === -1) {
+  //         continue
+  //     }
+
+  //     const rightIndex = right.lastIndexOf(number);
+  //     sum += (rightIndex - leftIndex + 1) * number
+  // }
+  // console.log(sum)
 });
